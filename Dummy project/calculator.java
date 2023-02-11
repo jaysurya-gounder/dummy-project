ae2514d10d0a09cc63ff3bea6022cfb7d86b37e9
Jaysurya Gounder <124020616+jaysurya-gounder@users.noreply.github.com>
2023-02-11 14:36:54 +0530

    uploaded file
    uploaded dummy file

---
+++ Dummy project/calculator.java
@@ -0,0 +1,47 @@
+import java.lang.*;
+import java.util.Scanner;
+
+class calculator
+{
+	public static void main(String args[])
+	{
+		System.out.println("1 For Addition");
+		System.out.println("2 For Subtraction");
+		System.out.println("3 For Multiplication");
+		System.out.println("4 For Division");
+		Scanner in = new Scanner(System.in);
+		int a, b, ch;
+		System.out.println("Enter the no");
+		ch = in.nextInt();
+		switch(ch)
+		{
+		case 1 : System.out.print("Enter the no");
+			a = in.nextInt();
+			System.out.println("Enter the no");
+			b= in.nextInt();
+			System.out.println("Addition: "+(a+b));
+			break;
+		case 2 : System.out.print("Enter the no");
+			a = in.nextInt();
+			System.out.println("Enter the no");
+			b= in.nextInt();
+			System.out.println("Subtraction: "+(a-b));
+			break;
+		case 3 : System.out.print("Enter the no");
+			a = in.nextInt();
+			System.out.println("Enter the no");
+			b= in.nextInt();
+			System.out.println("Multiplication: "+(a*b));
+			break;
+		case 4 : System.out.print("Enter the no");
+			a = in.nextInt();
+			System.out.println("Enter the no");
+			b= in.nextInt();
+			System.out.println("Division: "+(a/b));
+			break;
+		default : System.out.print("invalid");
+			break;	
+		}
+
+	}
+}
